04b082215088ea75b9528aae4083e5b00bb54e8e
willy jin <willy810110@gmail.com>
2023-05-07 18:27:12 +0800

    final ver

---
+++ passtry.py
@@ -0,0 +1,23 @@
+password = 'a123456'
+i = 3 #剩餘機會
+
+
+while i >= 0:
+
+    pastry = input('請輸入密碼:')
+
+    if pastry == password:
+
+        print("登入成功")
+
+        break
+
+    else:
+        i = i - 1
+        print("密碼錯誤!還有", i , "次機會")
+        if i == 0:
+            print('登入失敗，請洽客服人員')
+            break
+
+
+    
